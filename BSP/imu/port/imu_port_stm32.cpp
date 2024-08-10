c7ad36ddcd95cb152f7350783e08632a3ac6360c
eternal-echo <yuanyu@whut.edu.cn>
2024-08-10 22:56:12 +0800

    移植imu驱动

--- BSP/imu/port/imu_port_stm32.cpp
+++ BSP/imu/port/imu_port_stm32.cpp
@@ -6,6 +6,8 @@
 #include "main.h"
 #include "i2c.h"
 
+#include "MPU6050.hpp"
+
 #define TAG "imu"
 
 #include "elog.h"
@@ -14,12 +16,14 @@ static int i2c_write_byte_to_mem(uint8_t dev_addr, uint8_t reg_addr, uint8_t dat
 static int i2c_read_from_mem(uint8_t dev_addr, uint8_t reg_addr, uint32_t length, uint8_t *buf);
 static int imu_test(void);
 
+MPU6050 mpu6050(&imu_hi2c);
+
 /**
  * @brief  imu task 入口函数
  *
  */
 
-void imu_task_entry(void *argument) {
+extern "C" void imu_task_entry(void *argument) {
     elog_i(TAG, "imu task start");
 
     while (1) {
@@ -32,6 +36,22 @@ static int imu_test(void) {
     uint8_t data = 0;
     i2c_read_from_mem(0x68, 0x75, 1, &data);
     elog_i(TAG, "mpu6050 id: 0x%x", data);
+
+	/* imu */
+	// Init IMU.
+    do
+    {
+        mpu6050.Init();
+        osDelay(100);
+    } while (!mpu6050.testConnection());
+    mpu6050.InitFilter(200, 100, 50);
+
+	for(;;) {
+		mpu6050.Update(true);
+
+		elog_i(TAG, "Accel: %d %d %d", mpu6050.data.ax, mpu6050.data.ay);
+	}
+
     return 0;
 }
 
@@ -43,12 +63,9 @@ static int i2c_write_byte_to_mem(uint8_t dev_addr, uint8_t reg_addr, uint8_t dat
 	//退出中断级代码临界区
 	taskEXIT_CRITICAL_FROM_ISR(ret);
     return value;
-//	return HAL_I2C_Mem_Write(&hi2c2, self->dev_addr << 1, reg_addr, I2C_MEMADD_SIZE_8BIT, &data, 1, 0xFF);
 }
 
 static int i2c_read_from_mem(uint8_t dev_addr, uint8_t reg_addr, uint32_t length, uint8_t *buf) {
-// return HAL_I2C_Mem_Read(&hi2c2, self->dev_addr << 1, reg_addr, I2C_MEMADD_SIZE_8BIT, buf, length, 0xFF);
-//    return HAL_I2C_Mem_Read_DMA(&hi2c2, self->dev_addr << 1, reg_addr, I2C_MEMADD_SIZE_8BIT, buf, length);
 	int value = 0;
 	//进入中断级代码临界区
 	uint32_t ret = taskENTER_CRITICAL_FROM_ISR(); 
